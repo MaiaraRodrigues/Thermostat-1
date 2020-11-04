02581fe32e7dce224f07ca728a9e0e7c7271222a
Maiara Rodrigues <maiarinhah.eu@gmail.com>
2020-11-04 16:34:25 +0000

    Second test added for up function

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -7,7 +7,12 @@ describe('Thermostat', function() {
   })
 
   it('starts the thermostat at 20 degrees', function() {
-    expect(thermostat()).toEqual(20);
+    expect(thermostat.temperature()).toEqual(20);
+  });
+  
+  it('increases temperature with an up function', function() {
+    thermostat.up(20)
+    expect(thermostat.up(temperature)).toEqual(21);
   });
 
 
---
+++ src/Thermostat.js
@@ -0,0 +1,8 @@
+'use strict'
+
+class Thermostat{
+  temperature(){
+    return 20
+  }
+
+};
