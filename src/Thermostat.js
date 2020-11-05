529efaa74618ec899845a15be7ae1d57404400b4
Maiara Rodrigues <maiarinhah.eu@gmail.com>
2020-11-05 13:33:42 +0000

    Finished the business logic

--- src/Thermostat.js
+++ src/Thermostat.js
@@ -8,7 +8,9 @@ class Thermostat{
     this.temperature = this.DEFAULT_TEMPERATURE;
     this.powerSavingMode = true;
     this.MAX_LIMIT_PSM_ON = 25;
-    this.MAX_LIMIT_PSM_OFF = 32
+    this.MAX_LIMIT_PSM_OFF = 32;
+    this.MEDIUM_ENERGY_USAGE_LIMIT = 18;
+    this.HIGH_ENERGY_USAGE_LIMIT = 25;
   }
 
   getCurrentTemperature(){
@@ -56,6 +58,14 @@ class Thermostat{
       this.temperature = this.DEFAULT_TEMPERATURE;
     }
 
-
+    energyUsage() {
+      if (this.temperature < this.MEDIUM_ENERGY_USAGE_LIMIT) {
+        return 'low-usage';
+      }
+      if (this.temperature <= this.HIGH_ENERGY_USAGE_LIMIT) {
+        return 'medium-usage';
+      }
+      return 'high-usage';
+    }
 
 };
