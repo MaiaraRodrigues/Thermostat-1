f28967ac4fe20ccfdcc3d3c917902d9068c4346e
Maiara <maiarinhah.eu@gmail.com>
2021-04-09 16:54:51 +0100

    hosting again

--- public/javascript/src/interface.js
+++ public/javascript/src/interface.js
@@ -48,7 +48,7 @@ $(document).ready(function() {
     }) 
 
     function displayWeather(city) {
-      var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+      var url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city;
       var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
       var units = '&units=metric';
       $.get(url + token + units, function(data) {
